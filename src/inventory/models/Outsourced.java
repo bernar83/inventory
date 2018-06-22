7a4f4195d5a4b16d517fd2768e8b50b0e80ef6d0
Adrian Bernardo <nabernardo93@gmail.com>
2018-06-22 15:22:05 -0700

    Add outsourced class

---
+++ src/inventory/models/Outsourced.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package inventory.models;
+
+/**
+ *
+ * @author naber
+ */
+public class Outsourced extends Part{
+    private String companyName;
+    
+    public String getCompanyName() {
+        return this.companyName;
+    }
+    
+    public void setCompanyName(String name) {
+        this.companyName = name;
+    }
+}
