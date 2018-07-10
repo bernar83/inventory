218868d731e73362278fc57457b070c9c2d9764f
Adrian Bernardo <nabernardo93@gmail.com>
2018-07-10 12:08:10 -0700

    Open selected product to modify with product info

--- src/inventory/gui/MainScreenController.java
+++ src/inventory/gui/MainScreenController.java
@@ -112,11 +112,18 @@ public class MainScreenController implements Initializable {
     
     @FXML
     private void openModifyProduct(ActionEvent event) throws IOException {
-        Parent loadModProduct = FXMLLoader.load(getClass().getResource("/inventory/gui/ModifyProduct.fxml"));
-        Stage stage = new Stage();
-        stage.setScene(new Scene(loadModProduct));
-        ((Node)(event.getSource())).getScene().getWindow().hide();
-        stage.show();
+        Product selectedProduct = productsTable.getSelectionModel().getSelectedItem();
+        
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("ModifyProduct.fxml"));
+        Parent modPartParent = loader.load();
+        Scene modPartScene = new Scene(modPartParent);
+        Stage mainStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        ModifyProductController modController = loader.getController();
+        modController.openModProduct(selectedProduct);
+        mainStage.hide();
+        mainStage.setScene(modPartScene);
+        mainStage.show();
     }
     
     @FXML
--- src/inventory/gui/ModifyProductController.java
+++ src/inventory/gui/ModifyProductController.java
@@ -5,6 +5,7 @@
  */
 package inventory.gui;
 
+import inventory.models.Product;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -16,6 +17,7 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 /**
@@ -27,6 +29,28 @@ public class ModifyProductController implements Initializable {
     
     @FXML
     private Button closeModProductBtn;
+    @FXML
+    private TextField productId;
+    @FXML
+    private TextField productName;
+    @FXML
+    private TextField productInv;
+    @FXML
+    private TextField productPrice;
+    @FXML
+    private TextField productMax;
+    @FXML
+    private TextField productMin;
+    
+    @FXML
+    public void openModProduct(Product product) {
+        productId.setText(Integer.toString(product.getProductId()));
+        productName.setText(product.getName());
+        productInv.setText(Integer.toString(product.getInStock()));
+        productPrice.setText(Double.toString(product.getPrice()));
+        productMax.setText(Integer.toString(product.getMax()));
+        productMin.setText(Integer.toString(product.getMin()));
+    }
     
     @FXML
     private void closeModProduct(ActionEvent event) throws IOException {
